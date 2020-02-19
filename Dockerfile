539c65a51fecd96acd482dc46d7e5e4a2f83eddf
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-02-19 21:52:15 +0100

    feat: Re-implement with complete embed

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# Build container
+FROM golang:1.13.8 AS build
+
+WORKDIR /app
+
+ENV GO111MODULE=on
+
+RUN go install github.com/rakyll/statik
+RUN go install github.com/mholt/archiver/cmd/arc
+
+COPY . .
+
+RUN go generate ./...
+RUN go build -o ipxebuilderd main.go
+
+# Runner container
+FROM debian:buster
+
+RUN apt update
+RUN apt install -y gcc make git liblzma-dev
+
+COPY --from=build /app/ipxebuilderd /bin
+
+CMD /bin/ipxebuilderd
--- main.go
+++ main.go
@@ -1,21 +1,87 @@
-//go:generate sh -c "rm -rf vendor && mkdir -p vendor && git clone https://github.com/pojntfx/ipxe.git vendor/ipxe"
+//go:generate mkdir -p vendor/ipxe/dist
+//go:generate rm -rf vendor/ipxe/src
+//go:generate git clone https://github.com/pojntfx/ipxe.git vendor/ipxe/src
+//go:generate arc -overwrite archive vendor/ipxe/dist/ipxe.tar.gz vendor/ipxe/src
+//go:generate statik -src vendor/ipxe/dist
 
 package main
 
 import (
-	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
+
+	"github.com/mholt/archiver/v3"
+	"github.com/rakyll/statik/fs"
+	uuid "github.com/satori/go.uuid"
+
+	_ "github.com/pojntfx/ipxebuilderd/statik"
+)
+
+var (
+	basePath       = filepath.Join(os.TempDir(), "ipxebuilderd", uuid.NewV4().String())
+	archivePath    = filepath.Join(basePath, "archive")
+	execPath       = filepath.Join(basePath, "src", "src")
+	embedPath      = filepath.Join(execPath, "main.ipxe")
+	archiveOutPath = filepath.Join(archivePath, "ipxe.tar.gz")
+
+	platform  = "bin-x86_64-efi"
+	driver    = "ipxe"
+	extension = "efi"
+
+	embedScript = `#!ipxe
+
+autoboot`
 )
 
 func main() {
-	os.Chdir(filepath.Join("vendor", "ipxe", "src"))
-
-	out, err := exec.Command("make").CombinedOutput()
+	statikFS, err := fs.New()
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
 
-	fmt.Println(string(out))
+	data, err := fs.ReadFile(statikFS, "/ipxe.tar.gz")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := os.MkdirAll(archivePath, 0777); err != nil {
+		log.Fatal(err)
+	}
+
+	archiveOut, err := os.Create(archiveOutPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err := archiveOut.Write(data); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := archiver.Unarchive(archiveOutPath, basePath); err != nil {
+		log.Fatal(err)
+	}
+
+	embedFile, err := os.Create(embedPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err := embedFile.Write([]byte(embedScript)); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := os.Setenv("EMBED", embedPath); err != nil {
+		log.Fatal(err)
+	}
+
+	cmd := exec.Command("make", platform+"/"+driver+"."+extension)
+	cmd.Dir = execPath
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(string(out), err)
+	}
+
+	log.Println(string(out))
 }
